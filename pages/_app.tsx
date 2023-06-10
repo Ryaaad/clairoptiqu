79c45b84ba5b5b716ba253dfb035974ef74626d0
Ryaaad <87044522+Ryaaad@users.noreply.github.com>
2023-06-10 08:03:46 +0100

    url Bug fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,22 +7,6 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((ReactPixel) => {
-        ReactPixel.default.init('186022710735781'); // facebookPixelId
-        ReactPixel.default.pageView();
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.default.pageView();
-        });
-      })
-      .catch((error) => {
-        console.log('Error loading Facebook Pixel:', error);
-      });
-  }, []);
 
   return (
     <Provider store={store}>
